d5b35c4ae4464220a5a5e46f287c2ad665a0d666
Pablo Dominguez <dominguezpablo0998@gmail.com>
2021-04-06 00:45:19 -0300

    Primera carga al repositorio

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/components/app.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Holaa mundo</h1>
+      <h3>Gian se la come</h3>
+    </div>
+  );
+};
+
+export default App;
