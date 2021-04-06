cec1cdcd06de26d2b65fcfd1549cdf6d4cbfbf99
Pablo Dominguez <dominguezpablo0998@gmail.com>
2021-04-06 01:33:50 -0300

    Containers

--- Webpack.config.js
+++ Webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'boundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -82,6 +83,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
+    historyApiFallback: true,
     port: 3000,
   },
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/components/Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1 className="Header-title">Thorium store</h1>
+            <div className="Header-checkout">
+
+            </div>
+        </div>
+    );
+}
+
+export default Header;
