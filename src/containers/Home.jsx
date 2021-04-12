b20de8840d0c046960c42dd62b577f7f0c2230a0
Pablo Dominguez <dominguezpablo0998@gmail.com>
2021-04-12 01:23:03 -0300

    Deploy

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import initialState from '../initialState';
+import {Helmet} from 'react-helmet';
 import Products from '../components/Products';
 
 const Home = () => {
     return (
+        <Fragment> 
+            <Helmet>
+                <title>T Store- Productos</title>
+            </Helmet>
         <Products products={initialState.products} />
+        </Fragment>
     );
 }
 
