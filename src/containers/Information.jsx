7c02986ac38f6e3eeb7680da6e49cd17b8767c9b
Pablo Dominguez <dominguezpablo0998@gmail.com>
2021-04-11 23:17:15 -0300

    Actualizacion Paypal

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useRef, useContext} from 'react';
+import { Link} from 'react-router-dom';
 import '../styles/components/Information.css';
+import AppContext from '../context/AppContext';
 
-const Information = () => {
+const Information = ( { history }) => {
+const {state, addToBuyer} = useContext(AppContext);
+const form = useRef(null);
+
+const {cart} = state;
+
+
+const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const buyer = {
+        'name': formData.get('name'),
+        'email': formData.get('email'),
+        'address': formData.get('address'),
+        'appto': formData.get('appto'),
+        'city': formData.get('city'),
+        'country': formData.get('country'),
+        'state': formData.get('state'),
+        'cp': formData.get('cp'),
+        'phone': formData.get('phone'),
+    }
+
+    addToBuyer(buyer);
+    console.log(buyer);
+    history.push('/checkout/payment')
+}
+
     return (
         <div className="Information">
             <div className="Information-content">
@@ -10,7 +36,7 @@ const Information = () => {
                     <h2>Informacion de contacto:</h2>
                 </div>
                 <div className="Information-form">
-                    <form action="">
+                    <form ref={form}>
                         <input type="text" placeholder="Nombre Completo" name="name" />
                         <input type="text" placeholder="Email" name="email" />
                         <input type="text" placeholder="Direccion" name="address" />
@@ -23,22 +49,26 @@ const Information = () => {
                     </form>
                 </div>
                 <div className="Information-buttons">
-                    <div className="Information-back">Regresar</div>
-                    <div className="Information-next">
-                        <Link to="/checkout/payment">
-                           Pagar
+                    <div className="Information-back">
+                        <Link to="/checkout">
+                         Regresar
                         </Link>
+                        </div>
+                    <div className="Information-next">
+                        <button type="button" onClick={handleSubmit}>Pagar</button>
                     </div>
                 </div>
             </div>
             <div className="Information-sidebar">
                 <h3>Pedido:</h3>
-                <div className="Information-item">
+                {cart.map((item) => (
+                <div className="Information-item" key={item.title}>
                     <div className="Information-element">
-                        <h4>Item name</h4>
-                        <span>$10</span>
+                        <h4>{item.name}</h4>
+                        <span>$ {item.price}</span>
                     </div>
                 </div>
+                ))}
             </div>
         </div>
     );
--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '../styles/components/Payment.css';
+import {PayPalButton} from 'react-paypal-button';
 
-const Payment = () => {
+const Payment = ({history}) => {
+    const {state, addNewOrder} = useContext(AppContext);
+    const { cart, buyer } = state;
+  
+    const paypalOptions = {
+        clientId: 'AUPg8WXpLvFA5zg7F9viKkrYgRdWeXRYlY3eXcNvhcCWf1j39WaXfK-8xTKZNxBAuI0qJy2ucmkFeacS',
+        intent: 'capture',
+        currency: 'USD'
+    }
+
+    const buttonStyles = {
+        layout:'vertical',
+        shape: 'rect'
+    }
+
+    const handleSumTotal = () => {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+        const sum = cart.reduce(reducer, 0);
+        return sum;
+      }
+
+    const handlePaymentSuccess = (data) => {
+        console.log(data);
+        if(data.status == 'COMPLETED'){
+          const newOrder = {
+            buyer,
+            product: cart,
+            payment: data
+          }
+          addNewOrder(newOrder);
+          history.push('/checkout/success');
+        } 
+     }
+   
+
     return (
         <div className="Payment">
             <div className="Payment-content">
                 <h3>Resumen del pedido</h3>
+                {cart.map((item) => (
+                    <div className="Payment-item" key={item.title}>
+                        <h4>{item.title}</h4>
+                        <span>
+                            $ {item.price}
+                        </span>
+                    </div>
+                ))}
                 <div className="Payment-button">
-                    Boton de pago con paypal
+                    <PayPalButton
+                       paypalOptions={paypalOptions}
+                       buttonStyles={buttonStyles}
+                       amount={handleSumTotal()}
+                       onPaymentStart={() => console.log('Payment Started')}
+                       onPaymentSuccess= {data => handlePaymentSuccess(data)}
+                       onPaymentError={error => console.log(error)}
+                       onPaymentCancel={data => console.log(data)}
+                    />
                 </div>
             </div>
             <div />
